dc7c3e6cc60499d91398599027fe20c8f151a317
Balaji G <89681519+Balaji028@users.noreply.github.com>
2024-08-16 06:54:02 +0000

    basic

---
+++ DataTypes/basics.js
@@ -0,0 +1,96 @@
+
+
+// single line
+
+//console.log("hey"); //comment
+// console.log("check"); ctrl +?
+/*
+  console.log("multi")
+*/
+
+
+// data types
+
+// let a="hey"
+// console.log(typeof a);
+// const b =25
+// console.log(typeof b);
+// // b=30
+// // console.log(b); // constant cannot be changed
+// a=20.5
+// console.log(typeof a);
+
+
+/*
+let x =25 //integer
+console.log(typeof x);
+let y=20.5 //integer
+console.log(typeof y);
+let z =14253654125741258963257741255888n;
+console.log(typeof z);//big int ending with n
+let a =1n;
+console.log(typeof a);//big int ending with n
+*/
+
+
+/*
+let test =null
+console.log(test);  //null
+console.log(typeof test);//object
+
+let test1=undefined
+console.log(test1);//undefined
+console.log(typeof test1);//undefined
+*/
+
+console.log("number starts");
+
+
+let test3 =  Number(20);
+console.log(test3);
+console.log("The type of test3  is ", typeof(test3));
+
+let test4 =  new Number(26);
+console.log(test4);
+console.log(typeof(test4));
+
+
+console.log("number done");
+
+
+// boolean starts
+
+
+let test5 = Boolean()
+console.log(test5);
+console.log("the type is " , typeof(test5))
+
+let test6=new Boolean()
+console.log(test6);
+console.log(typeof(test6));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
