218d23c3358d46d514016fc42087458113e637de
Balaji G <89681519+Balaji028@users.noreply.github.com>
2024-08-18 07:56:12 +0000

    revision

---
+++ Revision/revision1.js
@@ -0,0 +1,169 @@
+console.log("+++++++++++++++++++++++++")
+console.log("started revision");
+
+// let x=10
+// console.log(`the value of x is ${x} and the type of x is ${typeof x}`);
+// x="changed reference"
+// console.log(typeof x);
+// console.log(`value of x is ${x}`);
+
+//  x = new Number(14)
+//  console.log(x);
+//  x=25
+//  console.log(x);
+
+
+//string
+// const test ="test"
+// console.log(`the value of test is ${test}`);
+// console.log(typeof test);
+// //boolean
+// const bool =true
+// console.log(typeof bool);
+// //symbol
+// const mysym=Symbol(true)
+// console.log(typeof mysym);
+// console.log("+++++++++");
+// console.log(mysym);
+
+
+
+// console.log("checkone");
+
+// const x =true
+// x=20
+// console.log(x);
+
+
+
+
+//array 
+// const myarr =[25,30,50,{name:"check",email:"check@example.com"}]
+// console.log(myarr[3]['name']);
+// console.log(myarr[3].name);
+
+// console.log("+");
+
+
+// console.log(myarr);
+// console.log(myarr[0]);
+// myarr.push(55)
+// myarr.push("hey")
+// console.log(myarr);
+// console.log(myarr.length);
+// let myarr1=new Array(4)
+// console.log(myarr1.length);
+// console.log(myarr1[0]);
+// console.log("++++++++++++++++");
+// myarr1[0]="hyd"
+// myarr1[1]="che"
+// myarr1[2]="ban"
+// myarr1[3]="kol"
+// console.log(myarr1[0]);
+// let myarr2=myarr.concat(myarr1)
+// console.log(myarr2);
+// console.log(myarr);
+// console.log(myarr1);
+// console.log("++++++++++++++++++++++++++++++++++++++++++++++++");
+// let jsone ={
+//     name:"example",
+//     email:"exampleadd@example.com"
+// }
+// myarr2.push(jsone)
+// console.log(myarr2);
+
+// console.log("+++++++++++++++++");
+// console.log("null and undefined practice");
+
+// let x ;
+// console.log(x);
+// console.log(`the value of x is ${x} and it's type is ${typeof x}`);
+
+// let y =null
+// console.log(y);
+// // console.log(`${y} ${typeof y}`);
+// console.log(typeof y); //object
+
+// console.log("++++++++++++++++++++++++++");
+
+// //number , string , null , undefined , symbol ,array
+// //object ,function 
+
+// let myarr=[1,2,3,4,5,6]
+// let myarr1=myarr
+// console.log(myarr);
+// console.log(myarr1);
+// myarr1=[1,2]
+// console.log("after changing the reference");
+// console.log(myarr);
+// console.log(myarr1);
+
+
+// console.log("++++++++++++++++++");
+// let x ={
+//     name:"test",
+//     email:"test@test.com"
+// }
+// let y =x
+// // console.log(`the value of x is ${x} \n the value of y is ${y}`);
+
+
+// console.log("the value before change is ",x)
+// console.log("the value before change is ",y);
+// console.log("now changing");
+// y.email="tester@tester.com"
+// console.log(x);
+// console.log(y);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+ 
+ 
+
+
+
+
+
+
